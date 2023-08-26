00eeacc74ddac9c80eff610b354fa668c8fc1fc5
Shahed595 <shahed.ahmed.cse@gmail.com>
2023-08-26 14:10:24 +0600

    register_part_1_done

--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import serializers,status
+from rest_framework.views import APIView
+from . import serializers
+from rest_framework.response import Response
 # Create your views here.
+class RegistrationView(APIView):
+     def post(self, request,):
+        serializer = serializers.RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
